8448007730c2bd708e81a4b7f2c9d6af237b31f4
Danielly - NVME <danielly.eng@hotmail.com>
2021-05-29 21:52:04 -0300

    Controllers com Parâmetros de Rotas

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,4 +11,10 @@ class ProductController extends Controller
         $products = ['Product 01', 'Product 02', 'Product 03'];
         return $products;
     }
+
+    public function show($id)
+    {
+        return "Exibindo o produto de id: {$id}";
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -95,3 +95,4 @@ Route::middleware([])->group(function() {
 });
 
 Route::get('products', 'ProductController@index')->name('products.index');
+Route::get('products/{id}', 'ProductController@show')->name('products.show');
