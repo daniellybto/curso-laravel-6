79da8ead9e1c7055f608c328a2c90bb2a480774b
Danielly - NVME <danielly.eng@hotmail.com>
2021-05-29 22:08:14 -0300

    Exemplo de rotas com verbos http com resource (apenas 1 linha)

--- routes/web.php
+++ routes/web.php
@@ -94,11 +94,14 @@ Route::middleware([])->group(function() {
     });  
 });
 
+//métodos sem resources
+// Route::delete('products/{id}', 'ProductController@destroy')->name('products.destroy');
+// Route::put('products/{id}', 'ProductController@update')->name('products.update');
+// Route::get('products/{id}/edit', 'ProductController@edit')->name('products.edit');
+// Route::get('products/create', 'ProductController@create')->name('products.create');
+// Route::get('products/{id}', 'ProductController@show')->name('products.show');
+// Route::get('products', 'ProductController@index')->name('products.index');
+// Route::post('products', 'ProductController@index')->name('products.index');
 
-Route::delete('products/{id}', 'ProductController@destroy')->name('products.destroy');
-Route::put('products/{id}', 'ProductController@update')->name('products.update');
-Route::get('products/{id}/edit', 'ProductController@edit')->name('products.edit');
-Route::get('products/create', 'ProductController@create')->name('products.create');
-Route::get('products/{id}', 'ProductController@show')->name('products.show');
-Route::get('products', 'ProductController@index')->name('products.index');
-Route::post('products', 'ProductController@index')->name('products.index');
+//método com Resources:
+Route::resource('products', 'ProductController');
