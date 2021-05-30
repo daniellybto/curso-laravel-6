88ab83029e2febb36b4ef71c1536b52b22adec95
Danielly - NVME <danielly.eng@hotmail.com>
2021-05-30 20:50:09 -0300

    Trabalhando com Factory (gerar dados fakes no BD) no Laravel

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,10 +12,19 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'name' => 'Danielly B',
-            'email' => 'dany@mail.com',
-            'password' => bcrypt('123456')
-        ]);
+        //o comando para rodar é o:
+        // php artisan db:seed
+        
+        // User::create([
+        //     'name' => 'Danielly B',
+        //     'email' => 'dany@mail.com',
+        //     'password' => bcrypt('123456')
+        // ]);
+
+        // criando registros fakes com Factories, 
+        // o primeiro parâmetro é qual tabela/classe eu quero gerar esses dados fakes;
+        // o segundo parâmetro (10) é a quantidade de registros que quero gerar, nesse caso coloquei apenas 10;
+        // o comando para se rodar é o: php artisan db:seed --class=UsersTableSeeder
+        factory(User::class, 10)->create();
     }
 }
